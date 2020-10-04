07906a5876372fb247497e1f7bcbee95641b3e90
MellYu <mel6273569@gmail.com>
2020-10-04 12:23:31 +0300

    Updates

---
+++ precache-manifest.fe9adeaa633781bb5d46c53a6d496a30.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ad524f93f24ee45284297fbb82934751",
+    "url": "/react-homework-02-phonebook/index.html"
+  },
+  {
+    "revision": "0fb5dda4cf24bbf1168d",
+    "url": "/react-homework-02-phonebook/static/css/main.4b851d01.chunk.css"
+  },
+  {
+    "revision": "87dbee3a13bfb90e2adb",
+    "url": "/react-homework-02-phonebook/static/js/2.fe989709.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-homework-02-phonebook/static/js/2.fe989709.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0fb5dda4cf24bbf1168d",
+    "url": "/react-homework-02-phonebook/static/js/main.1eb6b597.chunk.js"
+  },
+  {
+    "revision": "0e81bd0802b940635fab",
+    "url": "/react-homework-02-phonebook/static/js/runtime-main.a5568bcc.js"
+  }
+]);
